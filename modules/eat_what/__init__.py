4671ab3e3386d356db8819cf2130799beb1b646b
Redlnn <w731347477@gmail.com>
2023-09-02 20:02:15 +0800

    吃啥

---
+++ modules/eat_what/__init__.py
@@ -0,0 +1,28 @@
+import random
+from pathlib import Path
+
+from aiofile import async_open
+from avilla.core import Context, MessageReceived
+from avilla.twilight.twilight import RegexMatch, SpacePolicy, Twilight
+from graia.saya import Channel
+from graiax.shortcut.saya import dispatch, listen
+
+channel = Channel.current()
+
+channel.meta['name'] = '吃啥'
+channel.meta['author'] = ['Red_lnn']
+channel.meta['description'] = '[!！.]吃啥'
+
+
+async def get_food():
+    async with async_open(Path(Path(__file__).parent, 'foods.txt')) as afp:
+        foods = await afp.read()
+    return random.choice(foods.strip().split('\n'))
+
+
+@listen(MessageReceived)
+@dispatch(Twilight(RegexMatch(r'[!！.]吃啥').space(SpacePolicy.NOSPACE)))
+# @decorate(GroupPermission.require(), require_disable(channel.module))
+async def main(ctx: Context, event: MessageReceived):
+    food = await get_food()
+    await ctx.scene.send_message(f'吃{food}', reply=event.message)
